1ae9d29945c258d6e1f6456a1cedd9aa8f76ea34
Hashifhibatullah <93718271+Hashifhibatullah@users.noreply.github.com>
2024-05-19 13:50:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');	
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + '/src/index.html');
+	res.sendFile(__dirname + 'index.html');
 });
 
 const PORT = process.env.PORT || 8080;
